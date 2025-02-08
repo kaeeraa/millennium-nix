3dc61f4e8c276156d66480d3e69b275dca17ed62
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-09 00:09:54 +0300

    feat(default): implement default.nix
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

---
+++ default.nix
@@ -0,0 +1,34 @@
+{
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  python3,
+  ninja,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "millennium";
+  version = "2.17.2";
+
+  src = fetchFromGitHub {
+    owner = "shdwmtr";
+    repo = "millennium";
+    rev = "v${finalAttrs.version}";
+
+    fetchSubmodules = true;
+    hash = "";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    python3
+    ninja
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/shdwmtr/millennium";
+    description = "Millennium is an open-source low-code modding framework to create, manage and use themes/plugins for the desktop Steam Client without any low-level internal interaction or overhead.";
+    license = licenses.mit;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ kaeeraa ];
+  };
+})
