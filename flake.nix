32c6e01de061f61b0d531d8b77a56e751120c4fd
kaeeraa <kaeeraa@nebula-nook.ru>
2025-02-09 00:03:15 +0300

    feat(flake): implement flake (ayugram-desktop as template)
    Signed-off-by: kaeeraa <kaeeraa@nebula-nook.ru>

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Millennium Nix";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+  };
+  nixConfig = {
+    sandbox = true;
+    extra-substituters = [
+      "https://cache.garnix.io"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        millennium = pkgs.libsForQt5.callPackage ./default.nix { };
+      });
+    };
+}
